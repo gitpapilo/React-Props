ce287025386885dc679d539f71cbe5c36412bb45
gitpapilo <papixlawrence@gmail.com>
2023-08-17 13:29:05 +0100

    my first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PlayersList from './PlayersList';
+import "./App.css"
+
+
+const App = () => {
+  return (
+    <div className='player'>
+      <h1 >
+ Player List</h1>
+      <PlayersList />
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ src/players.js
@@ -0,0 +1,45 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentina",
+      jerseyNumber: 30,
+      age: 34,
+      imageUrl: "./images/messi.jpeg"
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Manchester United",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 36,
+      imageUrl: "./images/Ronaldo.jpeg"
+    },
+    {
+        name: "Neymar Jr",
+        team: "Paris Saint-Germain",
+        nationality: "Brazil",
+        jerseyNumber: 10,
+        age: 29,
+        imageUrl: "./images/Neymar.jpeg"
+      },
+      {
+        name: "Mbappe",
+        team: "Paris Saint-Germain",
+        nationality: "France",
+        jerseyNumber: 7,
+        age: 29,
+        imageUrl: "./images/mbappe.jpeg"
+      },
+      {
+    name: "Virgil van Dijk",
+    club: "Liverpool",
+    nationality: "Netherlands",
+    jerseyNumber: 4,
+    age: 30,
+    imageUrl: "./images/Virgil.jpeg"
+  },
+  ];
+  
+  export default players;
+  
